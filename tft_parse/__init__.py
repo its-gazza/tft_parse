3836ef7e44a9e9f40b91e208de76f1a9c5c098a4
its-gazza <gary.lkw95@gmail.com>
2021-01-21 23:10:02 +1100

    Added 4.5 data

--- tft_parse/__init__.py
+++ tft_parse/__init__.py
@@ -1,5 +1,5 @@
 __version__ = '0.0.1'
-current_tft_set = 4
+current_tft_set = '4.5'
 
 from .tft_api_class import MatchDto, InfoDto, MetadataDto, UnitDto, ParticipantDto, TraitDto
 from .misc import dict_add_count, route_region, regions
